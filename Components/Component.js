0c067a42e447c813d96eb72645cb4989c89eeff5
fabien-design <fabienrozier60@gmail.com>
2024-02-09 16:20:51 +0100

    add checkIfComponent Need to be refresh

--- Components/Component.js
+++ Components/Component.js
@@ -6,25 +6,30 @@ class Component {
         this.container = document.querySelector('#root');
         this.props = props;
         this.state = this.state || {};
+        this.prevProps = null;
+        this.prevState = null;
     }
 
     setState(newState){
         console.log(this.state, newState);
+        this.prevState = this.state;
         this.state = { ...this.state, ...newState };
-        this.refresh();
+        if (this.shouldRefresh()) {
+            this.refresh();
+        }
     }
 
-    // refresh() {
-    //     const container = this._container;
-    //     if (!container) return; // Do nothing if the component is not yet rendered
-    
-    //     // Create a new element with the updated content
-    //     const newElement = this.render();
-    
-    //     // Replace the existing content with the new content
-    //     container.innerHTML = '';
-    //     render(newElement, container);
-    // }
+    shouldRefresh() {
+        // Compare this.props with this.prevProps and this.state with this.prevState
+        // Return true if a refresh is needed and false otherwise
+        // This is a simple example and might not cover all cases
+        console.log('Previous Props:', this.prevProps);
+        console.log('Current Props:', this.props);
+        console.log('Previous State:', this.prevState);
+        console.log('Current State:', this.state);
+        return JSON.stringify(this.props) !== JSON.stringify(this.prevProps) ||
+               JSON.stringify(this.state) !== JSON.stringify(this.prevState);
+    }
 
     refresh() {
         const container = this._container;
@@ -43,8 +48,6 @@ class Component {
             console.error('Element with id', this.props.id, 'not found in container');
         }
     }
-    
-    
 
     mount(container) {
         this._container = container;
